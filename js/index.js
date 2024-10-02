80d3ade2fa184687954130c5b5daecb56e80c593
IsPekToP <inspektop1@gmail.com>
2024-10-02 11:49:17 +0200

    вставил шаблон,теперь надо разбираться,что там

--- js/index.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var bgVideo = document.getElementById('bgvideo');
-    if (window.innerWidth < 768) {
-        bgVideo.parentNode.removeChild(bgVideo); // Удаляем видео на мобильных устройствах
-    }
-});
