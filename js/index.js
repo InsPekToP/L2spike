8fbf3b49304249a12b9860aba842b7354ad999ae
IsPekToP <inspektop1@gmail.com>
2024-10-13 18:27:53 +0200

    удалил бургер меню,начал заново

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-document.querySelector('.burger').addEventListener('click',function(){
-    this.classList.toggle('active');
-    document.querySelector('.nav').classList.toggle('open');
-})
+// document.querySelector('.burger').addEventListener('click',function(){
+//     this.classList.toggle('active');
+//     document.querySelector('.nav').classList.toggle('open');
+// })
