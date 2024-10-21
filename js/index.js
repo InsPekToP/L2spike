b7c02c802c12435cb57a6b6bd7525208bf04c7d0
IsPekToP <inspektop1@gmail.com>
2024-10-21 18:12:39 +0200

    добавил переключение класса active при нажатии на лого

--- js/index.js
+++ js/index.js
@@ -1,6 +1,9 @@
 // Получаем все ссылки навигации
 const navLinks = document.querySelectorAll('.nav a');
 
+// Получаем лого
+const logo = document.querySelector('.logo');
+
 // Добавляем обработчик событий на каждую ссылку
 navLinks.forEach(link => {
     link.addEventListener('click', function() {
@@ -11,3 +14,13 @@ navLinks.forEach(link => {
         this.classList.add('active');
     });
 });
+
+// Добавляем обработчик событий на лого
+logo.addEventListener('click', function() {
+    // Убираем класс 'active' у всех ссылок
+    navLinks.forEach(link => link.classList.remove('active'));
+
+    // Добавляем класс 'active' к ссылке "Главная"
+    document.querySelector('.nav a[href="#home"]').classList.add('active');
+});
+
