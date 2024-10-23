1e333d5403e4a145fbd674efd981bc16dbce7ed6
IsPekToP <inspektop1@gmail.com>
2024-10-23 20:18:31 +0200

    добавил скрипт на скролл,подбить под html структуру

--- js/index.js
+++ js/index.js
@@ -24,3 +24,23 @@ logo.addEventListener('click', function() {
     document.querySelector('.nav a[href="#home"]').classList.add('active');
 });
 
+//Скрипт для скрола
+let section = document.querySelectorAll('section');
+let links = document.querySelectorAll('header nav a');
+
+window.onscroll = () =>{
+    section.forEach(sec =>{
+
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 100;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if(top >= offset && top < offset + height) {
+            links.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*='+ id +']').classList.add('active');
+            });
+        }
+    });
+};
