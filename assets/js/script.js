8c3c6a87f9380e6bd7f75423b2da2036986060dc
IsPekToP <inspektop1@gmail.com>
2025-02-13 21:16:15 +0100

    перенес весь шаблон сайта с папки Template_CodeWithSadee/GameSite_01

---
+++ assets/js/script.js
@@ -0,0 +1,50 @@
+//NAVBAR
+
+'use strict';
+
+const navbar = document.querySelector("[data-navbar]");
+const navbarLinks = document.querySelectorAll("[data-nav-link]");
+const navbarToggler = document.querySelector("[data-nav-toggler]");
+
+navbarToggler.addEventListener("click", function () {
+    navbar.classList.toggle("active");
+    this.classList.toggle("active");
+});
+
+
+for (let i= 0; i<navbarLinks.length; i++){
+    navbarLinks[i].addEventListener("click", function(){
+        navbar.classList.remove("active");
+        navbarToggler.classList.remove("active");
+    });
+}
+
+
+
+//SEARCH TOGGLE
+
+const searchToglers = document.querySelectorAll("[data-search-toggler]");
+const searchBox = document.querySelector("[data-search-box]");
+
+for (let i = 0; i<searchToglers.length; i++){
+    searchToglers[i].addEventListener("click", function(){
+        searchBox.classList.toggle("active");
+    });
+}
+
+
+
+// HEADER end BACK TO TOP
+
+const header = document.querySelector("[data-header]");
+const backTopBtn = document.querySelector("[data-back-top-btn]");
+
+window.addEventListener("scroll", function() {
+    if (window.scrollY >= 400){
+        header.classList.add("active");
+        backTopBtn.classList.add("active");
+    }else{
+        header.classList.remove("active");
+        backTopBtn.classList.remove("active");
+    }
+});
